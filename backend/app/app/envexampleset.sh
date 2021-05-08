4bd7070922410a9e34c2040ef11a7f068c73982f
mihail <mihailkirovv@gmail.com>
2021-05-08 16:35:07 +0300

    Fixed bugs in compose file

--- backend/app/app/envexampleset.sh
+++ backend/app/app/envexampleset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 truncate -s 0 .env.example;
-grep -E -o "^.*=" settings.py  | while read -r line ; do 
+grep -E -o "^[A-Za-z_]*=" .env  | while read -r line ; do 
 echo  $line >> .env.example; done
