ee27a264f9080759fea18a1d024eeba68e78c400
cchavano <58697129+cchavano@users.noreply.github.com>
2021-05-13 00:25:58 +0200

    Merge dev into deploy

--- app/frontend/webapp/bowtie/js/env.js
+++ app/frontend/webapp/bowtie/js/env.js
@@ -1,8 +1,4 @@
 const PROTOCOL = 'https';
 const API_SERVER_HOST = 'localhost';
-<<<<<<< HEAD
 const API_SERVER_PORT = '';
-=======
-const API_SERVER_PORT = '8000';
->>>>>>> dev
 const API_SERVER_URL = PROTOCOL + '://' + API_SERVER_HOST + ':' + API_SERVER_PORT;
