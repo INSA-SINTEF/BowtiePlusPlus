ca96b29ab4b34cc9561075fc9797a20d8ad35bbd
JuJuA13 <74596865+JuJuA13@users.noreply.github.com>
2022-03-27 18:41:20 +0200

    likelihood and helpjs

--- app/frontend/webapp/bowtie/js/help.js
+++ app/frontend/webapp/bowtie/js/help.js
@@ -1,29 +1,37 @@
-function getInfo(){
-    var fs = requirejs("fs");
-    const dictionnaire = new Map();
 
-    dictionnaire.set("greenA", document.getElementById('greenA').value);
-    dictionnaire.set("lgA", document.getElementById('lgA').value);
-    dictionnaire.set("yelA", document.getElementById('yelA').value);
-    dictionnaire.set("orA", document.getElementById('orA').value);
-    dictionnaire.set("redA", document.getElementById('redA').value);
+var dico_singleton = (function () {
+    var dictionnaire = new Map();
 
-    dictionnaire.set("greenO", document.getElementById('greenO').value);
-    dictionnaire.set("lgO", document.getElementById('lgO').value);
-    dictionnaire.set("yelO", document.getElementById('yelO').value);
-    dictionnaire.set("orO", document.getElementById('orO').value);
-    dictionnaire.set("redO", document.getElementById('redO').value);
+    function getInfo() {
 
-    dictionnaire.set("greenM", document.getElementById('greenM').value);
-    dictionnaire.set("lgM", document.getElementById('lgM').value);
-    dictionnaire.set("yelM", document.getElementById('yelM').value);
-    dictionnaire.set("orM", document.getElementById('orM').value);
-    dictionnaire.set("redM", document.getElementById('redM').value);
+        dictionnaire.set("greenA", document.getElementById('greenA').value);
+        dictionnaire.set("lgA", document.getElementById('lgA').value);
+        dictionnaire.set("yelA", document.getElementById('yelA').value);
+        dictionnaire.set("orA", document.getElementById('orA').value);
+        dictionnaire.set("redA", document.getElementById('redA').value);
 
-    dictionnaire.set("greenMo", document.getElementById('greenMo').value);
-    dictionnaire.set("lgMo", document.getElementById('lgMo').value);
-    dictionnaire.set("yelMo", document.getElementById('yelMo').value);
-    dictionnaire.set("orMo", document.getElementById('orMo').value);
-    dictionnaire.set("redMo", document.getElementById('redMo').value);
+        dictionnaire.set("greenO", document.getElementById('greenO').value);
+        dictionnaire.set("lgO", document.getElementById('lgO').value);
+        dictionnaire.set("yelO", document.getElementById('yelO').value);
+        dictionnaire.set("orO", document.getElementById('orO').value);
+        dictionnaire.set("redO", document.getElementById('redO').value);
 
-}
+        dictionnaire.set("greenM", document.getElementById('greenM').value);
+        dictionnaire.set("lgM", document.getElementById('lgM').value);
+        dictionnaire.set("yelM", document.getElementById('yelM').value);
+        dictionnaire.set("orM", document.getElementById('orM').value);
+        dictionnaire.set("redM", document.getElementById('redM').value);
+
+        dictionnaire.set("greenMo", document.getElementById('greenMo').value);
+        dictionnaire.set("lgMo", document.getElementById('lgMo').value);
+        dictionnaire.set("yelMo", document.getElementById('yelMo').value);
+        dictionnaire.set("orMo", document.getElementById('orMo').value);
+        dictionnaire.set("redMo", document.getElementById('redMo').value);
+        console.log(dictionnaire);
+
+        var txtFile = new File(dico.txt);
+        txtFile.writeln(txtFile, dictionnaire);
+        txtFile.close();
+        console.log("done")
+    }
+})
